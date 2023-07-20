1dddfd7f97c89e1433e2732ff1ba09b5d70b1031
Jackson <jackson@jacksonmoody.com>
2023-07-19 23:29:08 -0600

    Initial Commit

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const { App } = require("@slack/bolt");
+const { App, subtype } = require("@slack/bolt");
 require("dotenv").config();
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// All the room in the world for your code
+app.message(subtype("file_share"), ({ event, logger }) => {
+    console.log(event);
+});
 
 (async () => {
   await app.start(process.env.PORT || 3000);
