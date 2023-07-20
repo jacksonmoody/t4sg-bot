aff123bc746aadff4088b8eac8dbe1a9fe703723
Jackson <jackson@jacksonmoody.com>
2023-07-20 00:55:00 -0600

    Fixed env variables

--- api/_constants.js
+++ api/_constants.js
@@ -4,5 +4,5 @@ export const token = process.env.SLACK_BOT_TOKEN;
 export const signingSecret = process.env.SLACK_SIGNING_SECRET;
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY
 );
