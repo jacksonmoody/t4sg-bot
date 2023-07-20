392700eb4f9d74b07c3d9623662046c9423100cc
Jackson <jackson@jacksonmoody.com>
2023-07-20 01:48:45 -0600

    Filtering

--- api/_new_message.js
+++ api/_new_message.js
@@ -1,9 +1,10 @@
-import { token } from './_constants'
+import { token } from "./_constants";
 export async function new_message(req, res) {
   let event = req.body.event;
-  console.log(event);
   try {
-    //await publishMessage("C05JLAH7U80", "Hello, World", res);
+    if (event.type == "file_shared") {
+      await publishMessage("C05JLAH7U80", "Hello, World!", res);
+    }
   } catch (e) {
     console.log(e);
   }
@@ -13,23 +14,23 @@ async function publishMessage(id, payload, res) {
   const message = {
     channel: id,
     text: payload,
-  }
+  };
   try {
-    const url = 'https://slack.com/api/chat.postMessage'
+    const url = "https://slack.com/api/chat.postMessage";
     const response = await fetch(url, {
-      method: 'post',
+      method: "post",
       headers: {
-        'Content-Type': 'application/json; charset=utf-8',
+        "Content-Type": "application/json; charset=utf-8",
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(message),
-    })
-    const data = await response.json()
-    res.json({ ok: true })
+    });
+    const data = await response.json();
+    res.json({ ok: true });
   } catch (err) {
     res.send({
-      response_type: 'ephemeral',
+      response_type: "ephemeral",
       text: `${err}`,
-    })
+    });
   }
 }
