a7204bda6514633d7d5499816727e7443175ed19
Jackson <jackson@jacksonmoody.com>
2023-07-20 01:53:19 -0600

    Disable check

--- api/events.js
+++ api/events.js
@@ -14,14 +14,7 @@ export default async function events(req, res) {
     await challenge(req, res);
   } else if (validate(req, signingSecret)) {
     if (type === "event_callback") {
-      const event_type = req.body.event.type;
-      switch (event_type) {
-        case "message":
-          await new_message(req, res);
-          break;
-        default:
-          break;
-      }
+      await new_message(req, res);
     }
   }
 }
