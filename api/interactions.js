5270341757e3df45e6a2aa493c06c1d7c7a82e0d
Jackson <jackson@jacksonmoody.com>
2023-07-21 17:47:47 -0600

    Added debugging

--- api/interactions.js
+++ api/interactions.js
@@ -11,9 +11,19 @@ export default async function interactions(req, res) {
   if (type === "url_verification") {
     await challenge(req, res);
   } else if (validate(req, signingSecret)) {
-    const data = req.body.payload;
-    console.log(data);
-    res.status(200).send({});
+    try {
+      console.log(req.body.payload);
+      const payload = JSON.parse(req.body.payload);
+      const type = payload.type;
+      if (type === "block_actions") {
+        res.status(200).send({ok: true});
+      } else {
+        res.status(400).send({ error: "Invalid event type" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({ error: "Invalid request" });
+    }
   } else {
     res.status(400).send({ error: "Invalid request" });
   }
