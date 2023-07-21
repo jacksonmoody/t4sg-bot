159a97a24ae18f20eea6d5699b140d257e9acc81
Jackson <jackson@jacksonmoody.com>
2023-07-20 21:54:07 -0600

    Added image upload

--- api/_constants.js
+++ api/_constants.js
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const token = process.env.SLACK_BOT_TOKEN;
 export const signingSecret = process.env.SLACK_SIGNING_SECRET;
+export const classificationToken = process.env.CLASSIFICATION_TOKEN;
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY,
--- api/_new_message.js
+++ api/_new_message.js
@@ -1,17 +1,21 @@
 import { token } from "./_constants";
+import { classificationToken } from "./_constants";
 import { supabase } from "./_constants";
+const fs = require("fs");
+
 export async function new_message(req, res) {
   let event = req.body.event;
   try {
     if (event.type == "file_shared") {
       await publishMessage("C05JLAH7U80", "New Snipe Posted!", res);
       const file = await fetchFile(event.file_id);
+      await downloadImage(file.file.url_private_download, file.file.id);
       const { error } = await supabase.from("snipes").insert([
         {
           user_id: event.user_id,
-          image: file.file.url_private,
-          description: file.file.title
-        }
+          image: file.file.url_private_download,
+          description: file.file.title,
+        },
       ]);
       if (error) {
         console.log(error);
@@ -24,7 +28,7 @@ export async function new_message(req, res) {
     } else {
       res.send({
         text: "Unsupported event type",
-      })
+      });
     }
   } catch (e) {
     res.send({
@@ -66,4 +70,43 @@ async function fetchFile(id) {
   });
   const data = await response.json();
   return data;
+}
+
+async function downloadImage(url, filename) {
+  let writeStream = fs.createWriteStream(`/tmp/${filename}.pdf`);
+  const response = await fetch(url, {
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  response.pipe(writeStream);
+  writeStream.on("finish", () => {
+    writeStream.close();
+    const fileContent = fs.readFileSync(`/tmp/${filename}.pdf`);
+    const params = {
+      image: fileContent,
+    };
+    fetch("https://api.imagga.com/v2/uploads", {
+      method: "post",
+      headers: {
+        Authorization: `Basic ${classificationToken}`,
+      },
+      body: params,
+    }).then((response) => {
+      console.log(response);
+    });
+  });
+}
+
+async function getClassification(url) {
+  const baseURL = "https://api.imagga.com/v2/tags";
+  const response = await fetch(baseURL + "?image_url=" + url, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: `Basic ${classificationToken}`,
+    },
+  });
+  return response;
 }
