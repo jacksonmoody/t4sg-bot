5f3d647255ab660a31b10695a361a55873579a23
Jackson <jackson@jacksonmoody.com>
2023-07-21 17:44:12 -0600

    Updated

--- api/interactions.js
+++ api/interactions.js
@@ -1,12 +1,20 @@
+import { challenge } from "./_challenge";
+import { validate } from "./_validate";
+import { signingSecret } from "./_constants";
+
 export default async function interactions(req, res) {
-  try {
-    const data = await req.body.payload.json();
-    console.log(data);
-    res.status(200).send({});
-    return;
-  } catch (err) {
-    console.log(err);
+  if (!req.body) {
     res.status(400).send({ error: "Invalid request" });
     return;
+  }
+  const type = req.body.type;
+  if (type === "url_verification") {
+    await challenge(req, res);
+  } else if (validate(req, signingSecret)) {
+    const data = req.body.payload;
+    console.log(data);
+    res.status(200).send({});
+  } else {
+    res.status(400).send({ error: "Invalid request" });
   }
 }
