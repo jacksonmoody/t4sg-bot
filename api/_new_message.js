de825cf0e82b0a6342ef7e1c04c3799a1319b5b5
Jackson <jackson@jacksonmoody.com>
2023-07-20 23:09:01 -0600

    Image blob

--- api/_new_message.js
+++ api/_new_message.js
@@ -9,7 +9,11 @@ export async function new_message(req, res) {
     if (event.type == "file_shared") {
       await publishMessage("C05JLAH7U80", "New Snipe Posted!", res);
       const file = await fetchFile(event.file_id);
-      const fileID = await downloadImage(file.file.url_private_download, file.file.id, res);
+      const fileID = await downloadImage(
+        file.file.url_private_download,
+        file.file.id,
+        res
+      );
       await publishMessage("C05JLAH7U80", fileID, res);
       const { error } = await supabase.from("snipes").insert([
         {
@@ -74,7 +78,6 @@ async function fetchFile(id) {
 }
 
 async function downloadImage(url, filename, res) {
-  let writeStream = fs.createWriteStream(`/tmp/${filename}.png`);
   try {
     const response = await fetch(url, {
       method: "get",
@@ -82,34 +85,31 @@ async function downloadImage(url, filename, res) {
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log(response);
-    response.body.pipe(writeStream);
-    writeStream.on("finish", () => {
-      const fileContent = fs.readFileSync(`/tmp/${filename}.png`);
-      const params = {
-        image: fileContent,
-      };
-      publishMessage("C05JLAH7U80", "File Downloaded", res);
-      fetch("https://api.imagga.com/v2/uploads", {
-        method: "post",
-        headers: {
-          Authorization: `Basic ${classificationToken}`,
-        },
-        body: params,
-      })
-        .then((response) => {
-          response.json().then((data) => {
-            const upload_id = data.result.upload_id;
-            publishMessage("C05JLAH7U80", upload_id, res);
-            return upload_id;
-          });
-        })
-        .catch((err) => {
-          res.send({
-            text: `${err}`,
-          });
+    const imageBlob = await response.blob();
+    console.log(imageBlob);
+    const params = {
+      image: fileContent,
+    };
+    publishMessage("C05JLAH7U80", "File Downloaded", res);
+    fetch("https://api.imagga.com/v2/uploads", {
+      method: "post",
+      headers: {
+        Authorization: `Basic ${classificationToken}`,
+      },
+      body: params,
+    })
+      .then((response) => {
+        response.json().then((data) => {
+          const upload_id = data.result.upload_id;
+          publishMessage("C05JLAH7U80", upload_id, res);
+          return upload_id;
         });
-    });
+      })
+      .catch((err) => {
+        res.send({
+          text: `${err}`,
+        });
+      });
   } catch (err) {
     console.log(err);
     res.send({
