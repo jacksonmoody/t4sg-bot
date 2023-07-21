6326a22dbe14ce823628e95ff37b088d1a9a8800
Jackson <jackson@jacksonmoody.com>
2023-07-21 17:41:33 -0600

    Updated edge function

--- api/interactions.js
+++ api/interactions.js
@@ -1,17 +1,12 @@
-import { challenge } from "./_challenge";
-import { validate } from "./_validate";
-import { signingSecret } from "./_constants";
-
 export default async function interactions(req, res) {
-  if (!req.body) {
-    res.status(400).send({ error: "Invalid request" });
-    return;
-  }
   try {
     const data = await req.body.payload.json();
     console.log(data);
     res.status(200).send({});
+    return;
   } catch (err) {
+    console.log(err);
     res.status(400).send({ error: "Invalid request" });
+    return;
   }
 }
