facb88de5967441f2c9d28189d338becd05956c1
Jackson <jackson@jacksonmoody.com>
2023-07-21 17:49:46 -0600

    Removed checks

--- api/interactions.js
+++ api/interactions.js
@@ -1,30 +1,15 @@
-import { challenge } from "./_challenge";
-import { validate } from "./_validate";
-import { signingSecret } from "./_constants";
-
 export default async function interactions(req, res) {
-  if (!req.body) {
-    res.status(400).send({ error: "Invalid request" });
-    return;
-  }
-  const type = req.body.type;
-  if (type === "url_verification") {
-    await challenge(req, res);
-  } else if (validate(req, signingSecret)) {
-    try {
-      console.log(req.body.payload);
-      const payload = JSON.parse(req.body.payload);
-      const type = payload.type;
-      if (type === "block_actions") {
-        res.status(200).send({ok: true});
-      } else {
-        res.status(400).send({ error: "Invalid event type" });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(400).send({ error: "Invalid request" });
+  try {
+    console.log(req.body.payload);
+    const payload = JSON.parse(req.body.payload);
+    const type = payload.type;
+    if (type === "block_actions") {
+      res.status(200).send({ ok: true });
+    } else {
+      res.status(400).send({ error: "Invalid event type" });
     }
-  } else {
+  } catch (err) {
+    console.log(err);
     res.status(400).send({ error: "Invalid request" });
   }
 }
