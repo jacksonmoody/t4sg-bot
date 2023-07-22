7bfe2f1a80c3a425d62427bc8d8a9d9bc5e4ab03
Jackson <jackson@jacksonmoody.com>
2023-07-21 18:04:47 -0600

    Add DM

--- api/interactions.js
+++ api/interactions.js
@@ -1,9 +1,14 @@
+import { publishMessage } from "./_utils";
+import { adminID, adminUsername } from "./_constants";
+
 export default async function interactions(req, res) {
   try {
-    console.log(req.body.payload);
     const payload = JSON.parse(req.body.payload);
     const type = payload.type;
     if (type === "block_actions") {
+      const user = payload.user.username;
+      const value = payload.message.blocks[1].elements[0].value;
+      await publishMessage(adminID, "Hey, @" + user + "! " + user + " wants to contest this snipe: " + value);
       res.status(200).send({ ok: true });
     } else {
       res.status(400).send({ error: "Invalid event type" });
