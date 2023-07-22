8c4af3797dccd58216ca32e02c2de86f11167fe5
Jackson <jackson@jacksonmoody.com>
2023-07-21 23:52:28 -0600

    Redo auth

--- api/auth.js
+++ api/auth.js
@@ -2,19 +2,17 @@ import { clientID, clientSecret } from "./_constants";
 export default async function auth(req, res) {
   const code = req.query.code;
   const url = "https://slack.com/api/oauth.v2.access";
-  const response = await fetch(url, {
-    method: "post",
+  let headers = new Headers();
+  headers.set('Authorization', 'Basic ' + Buffer.from(clientID + ":" + clientSecret).toString('base64'));
+  const response = await fetch(url + "?code=" + code, {
+    method: "POST",
     headers: {
       "Content-Type": "application/json; charset=utf-8",
     },
-    body: JSON.stringify({
-      client_id: clientID,
-      client_secret: clientSecret,
-      code: code,
-    }),
   });
-    const data = await response.json();
-    const token = data.access_token;
-    console.log(token);
-    res.status(200).send("Success! You can close this window now.");
+  console.log(response);
+  const data = await response.json();
+  const token = data.access_token;
+  console.log(token);
+  res.status(200).send("Success! You can close this window now.");
 }
