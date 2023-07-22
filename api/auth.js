895d20b93fe432ad8f318875e47260d832f18a6f
Jackson <jackson@jacksonmoody.com>
2023-07-22 00:06:46 -0600

    Update auth

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,8 @@
-import { clientID } from "./_constants";
+import { clientID, clientSecret } from "./_constants";
 export default async function auth(req, res) {
   const code = req.query.code;
   const url = "https://slack.com/api/oauth.v2.access";
-  let headers = new Headers();
-  headers.set('Authorization', 'Basic ' + clientID);
-  const response = await fetch(url + "?code=" + code, {
+  const response = await fetch(url + "?client_id=" + clientID + "&client_secret=" + clientSecret + "&code=" + code, {
     method: "POST",
     headers: {
       "Content-Type": "application/json; charset=utf-8",
