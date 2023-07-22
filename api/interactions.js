7f594a50f3e169190d36c23c1c9fdd62ebe65452
Jackson <jackson@jacksonmoody.com>
2023-07-21 18:37:16 -0600

    Removed verification

--- api/interactions.js
+++ api/interactions.js
@@ -1,35 +1,29 @@
 import { publishMessage } from "./_utils";
-import { adminID, adminUsername, signingSecret } from "./_constants";
-import { validate } from "./_validate";
+import { adminID, adminUsername } from "./_constants";
 
 export default async function interactions(req, res) {
-  if (validate(req, signingSecret)) {
-    try {
-      const payload = JSON.parse(req.body.payload);
-      const type = payload.type;
-      if (type === "block_actions") {
-        const user = payload.user.username;
-        const value = payload.message.blocks[1].elements[0].value;
-        console.log(value);
-        await publishMessage(
-          adminID,
-          "Hey, @" +
-            adminUsername +
-            "! " +
-            user +
-            " wants to contest this snipe: " +
-            value
-        );
-        res.status(200).send({ ok: true });
-      } else {
-        res.status(400).send({ error: "Invalid event type" });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(400).send({ error: "Invalid request" });
+  try {
+    const payload = JSON.parse(req.body.payload);
+    const type = payload.type;
+    if (type === "block_actions") {
+      const user = payload.user.username;
+      const value = payload.message.blocks[1].elements[0].value;
+      console.log(value);
+      await publishMessage(
+        adminID,
+        "Hey, @" +
+          adminUsername +
+          "! " +
+          user +
+          " wants to contest this snipe: " +
+          value
+      );
+      res.status(200).send({ ok: true });
+    } else {
+      res.status(400).send({ error: "Invalid event type" });
     }
-  } else {
-    console.log("Invalid request");
+  } catch (err) {
+    console.log(err);
     res.status(400).send({ error: "Invalid request" });
   }
 }
